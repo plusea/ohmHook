0b09f2a9fbe580a502e2bf9e4b76b94cdebdeee8
Admar Schoonen <admar@familieschoonen.nl>
2017-08-06 23:46:38 +0200

    Attempt at improving resistance measurement (untested!)

--- ohmHandle_V4segWS/ohmHandle_V4segWS.ino
+++ ohmHandle_V4segWS/ohmHandle_V4segWS.ino
@@ -9,6 +9,7 @@
 */
 
 #include <LedDisplay.h>
+#include <stdint.h>
 
 // Define pins for the LED display:
 #define dataPin 1              // connects to the display's data in
@@ -18,12 +19,15 @@
 #define reset 9                // the display's reset pin
 #define displayLength 4        // number of characters in the display
 LedDisplay myDisplay = LedDisplay(dataPin, registerSelect, clockPin, enable, reset, displayLength); // create am instance of the LED display library:
-float brightness = 15;        // screen brightness
+float brightness = 15;         // screen brightness
 
 #define buttonPin 5
 #define potMinus 6
 #define analogPin 7
 #define speakerPin 10
+#define potValue 500000
+#define nSamplesMax 20         // maximum number of samples; must be less than 32 to prevent overflow
+
 
 // Conversion of ADC value to mV. Should be calibrated for each device?
 // Should also be made independent of battery voltage by reading 1.1V reference voltage first?
@@ -49,7 +53,8 @@ int lastButtonState = 0; // stores the previous state of the button
 mode_e mode = mode_analog_read_init;            // mode will store the current blinking mode (0 - 3)
 int myDirection = 1;           // direction of scrolling.  -1 = left, 1 = right.
 int analogValue;
-float resistanceValue;
+uint32_t potBotValue;
+uint32_t resistanceValue;
 float soundValue;
 
 
@@ -100,36 +105,44 @@ void handle_synth(void) {
 }
 
 void handle_resistance_init(void) {
-  // READ POT //
+  uint8_t n;
+  
   pinMode(potMinus, OUTPUT);
   digitalWrite(potMinus, LOW);
+  analogRead(analogPin); // always discard first measurement after switching channel
+  
+  analogValue = 0;
+  for (n = 0; n < nSamplesMax; n++)
+    analogValue += analogRead(analogPin);
+
+  potBotValue = map(analogValue, 0, 1023 * nSamplesMax, 0, potValue);
+  potBotValue = constrain(potBotValue, 0, potValue);
+  
   mode = mode_resistance;
 }
 
 void handle_resistance(void) {
+  char unit = 'R';
+  
   analogValue = analogRead(analogPin);
-  resistanceValue = map(analogValue, 1023, 0, 0, 500000);
+  resistanceValue = (uint32_t) ((potValue - potBotValue) * analogValue / (1023 - analogValue));
+  resistanceValue = constrain(resistanceValue, 0, potValue);
+  
   // DISPLAY //
   myDisplay.setCursor(0);  // set the cursor to 1
-  //myDisplay.print(analogValue, DEC);
 
-  if (resistanceValue >= 450000 && resistanceValue < 500000) myDisplay.print("500K");
-  if (resistanceValue >= 400000 && resistanceValue < 450000) myDisplay.print("450K");
-  if (resistanceValue >= 350000 && resistanceValue < 400000) myDisplay.print("400K");
-  if (resistanceValue >= 300000 && resistanceValue < 350000) myDisplay.print("350K");
-  if (resistanceValue >= 250000 && resistanceValue < 300000) myDisplay.print("300K");
-  if (resistanceValue >= 200000 && resistanceValue < 250000) myDisplay.print("250K");
-  if (resistanceValue >= 100000 && resistanceValue < 200000) myDisplay.print("200K");
-  if (resistanceValue >= 50000 && resistanceValue < 100000) myDisplay.print("100K");
-  if (resistanceValue >= 40000 && resistanceValue < 50000) myDisplay.print(" 50K");
-  if (resistanceValue >= 30000 && resistanceValue < 40000) myDisplay.print(" 40K");
-  if (resistanceValue >= 20000 && resistanceValue < 30000) myDisplay.print(" 30K");
-  if (resistanceValue >= 10000 && resistanceValue < 20000) myDisplay.print(" 20K");
-  if (resistanceValue >= 5000 && resistanceValue < 10000) myDisplay.print(" 10K");
-  if (resistanceValue >= 1000 && resistanceValue < 5000) myDisplay.print("  5K");
-  if (resistanceValue >= 500 && resistanceValue < 1000) myDisplay.print("  1K");
-  if (resistanceValue >= 100 && resistanceValue < 500) myDisplay.print(" 500");
-  if (resistanceValue >= 0 && resistanceValue < 100) myDisplay.print("<100");
+  if (resistanceValue >= 1000) {
+    resistanceValue = resistanceValue / 1000;
+    unit = 'K';
+  }
+  if (resistanceValue < 10) {
+    myDisplay.print(" ");
+  }
+  if (resistanceValue < 100) {
+    myDisplay.print(" ");
+  }
+  myDisplay.print(resistanceValue);
+  myDisplay.print(unit);
 }
 
 void handle_volt_init(void) {
